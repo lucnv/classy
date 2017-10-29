7678411639cf56aec2eb2f4e3df0760b6d58cbdb
lucnv <nguyenluc312@gmail.com>
2017-10-29 20:34:58 +0700

    change-order-reviews-on-branch-page-reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ApplicationRecord
+  SORTING_OPTIONS = [{name: :newest, value: "created_at asc"},
+    {name: :most_voting, value: "vote_points_cached desc"}
+  ]
+
   belongs_to :user
   belongs_to :branch
   has_many :comments, dependent: :destroy
@@ -11,6 +15,7 @@ class Review < ApplicationRecord
       ON #{Review.table_name}.id = #{Vote.table_name}.review_id AND #{Vote.table_name}.user_id = #{user.id}")
       .select("#{Review.table_name}.*, #{Vote.table_name}.vote_type AS voted_type")
   end
+  scope :most_voting, ->{order vote_points_cached: :desc}
 
   enum status: [:unverified, :verified]
 
@@ -18,5 +23,13 @@ class Review < ApplicationRecord
 
   def update_vote_points_cached
     update_columns vote_points_cached: Vote.points_of_review(self)
+  end
+
+  class << self
+    # Add sorting scopes to Ransack for approval_rate computed column
+    #
+    def ransackable_scopes(auth_object = nil)
+      [:recent_created, :most_voting]
+    end
   end
 end
